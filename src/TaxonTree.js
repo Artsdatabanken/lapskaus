f8dfb2cdcbb2b14e56729753fba48d2723705535
amned <amund.nedland@norconsult.com>
2017-10-30 12:54:17 +0100

    Fjernet ubrukt referanse i TaxonTree

--- src/TaxonTree.js
+++ src/TaxonTree.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
