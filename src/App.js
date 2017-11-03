9982f2af8876c6632cded22bdd87a279c4d1ec4e
Bjørn Reppen <bjreppen@gmail.com>
2017-11-03 07:18:03 +0100

    Change web page title 'React App'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import TaxonList from './TaxonListContainer/TaxonListContainer'
 import {resetComponentIds} from './componentid'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import './App.css'
 
 class App extends Component {
   render() {
