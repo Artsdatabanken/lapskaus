d4d765ea2d3c51cc0643ee4a4f021a3048bcfe44
Bjørn Reppen <bjreppen@gmail.com>
2017-11-06 16:23:31 +0100

    Static observation map sample

--- src/map/EnvironmentFilterHeatMap/EnvironmentFilterHeatMap.stories.js
+++ src/map/EnvironmentFilterHeatMap/EnvironmentFilterHeatMap.stories.js
@@ -3,12 +3,12 @@ import renderer from 'react-test-renderer'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
-import EnvironmentMap from './EnvironmentMap'
+import EnvironmentFilterHeatMap from './EnvironmentFilterHeatMap'
 
 const textureWidth = 1237
 const textureHeight = 1552
 
-storiesOf('Map', module)
+storiesOf('Map environment filtered', module)
 .addDecorator(withKnobs)
 .add('alt', () => {
     return (
@@ -42,7 +42,7 @@ const Map = (props) => {
   step: 10.00,
 };
 
-  const alpha = number('alpha', 0.5, options);
+  const alpha = number('alpha', 1.0, options);
   const filterMin = number('filterMin', props.filterMin, elevationOptions);
   const filterMax = number('filterMax', props.filterMax, elevationOptions);
   return (
@@ -53,7 +53,7 @@ const Map = (props) => {
     }}
   >
     <h3>Taxon #{props.taxonId}: range {filterMin}-{filterMax}</h3>
-    <EnvironmentMap {...props}
+    <EnvironmentFilterHeatMap {...props}
       alpha={alpha} filterMin={filterMin} filterMax={filterMax}/>
   </div>
 )
---
+++ src/map/ObservationStaticHeatmap/ObservationStaticHeatmap.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import animationLoop from './animationLoop'
+
+class ObservationStaticHeatmap extends Component {
+  componentDidMount() {
+    this.setProps(this.props)
+    animationLoop.stop()
+    animationLoop.start({ canvas: 'map-canvas' })
+    window.setTimeout(()=>this.stopAnimation(), 5000);
+  }
+
+  stopAnimation() {
+    animationLoop.stop();
+    animationLoop._stopped=true
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setProps(nextProps)
+  }
+
+  setProps(nextProps) {
+    animationLoop.taxonId = nextProps.taxonId
+    animationLoop.filterMin = nextProps.filterMin
+    animationLoop.filterMax = nextProps.filterMax
+    animationLoop.alpha = nextProps.alpha
+  }
+
+  componentWillUnmount() {
+    animationLoop.stop()
+  }
+
+  render() {
+    return <canvas id="map-canvas" style={{width: '100%', height: '100%'}}/>
+  }
+}
+
+ObservationStaticHeatmap.propTypes = {
+  alpha: PropTypes.number,
+  taxonId: PropTypes.number,
+  filterMin: PropTypes.number,
+  filterMax: PropTypes.number
+}
+
+export default ObservationStaticHeatmap
